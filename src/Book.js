03abaaab997087166cce342a46d96db5ea98af6b
mcanas <mcanas@cargurus.com>
2017-07-10 18:02:23 -0400

    Removing the value='none' from the shelf changers disabled option

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ const Book = props => {
           <select
             value={shelf}
             onChange={e => onUpdateBookShelf(book, e.target.value)}>
-            <option value="none" disabled>Move to...</option>
+            <option disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
             <option value="read">Read</option>
